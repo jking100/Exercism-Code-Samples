77f11238ea940653f0286389517bf3faa1f97b25
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-19 17:59:42 -0600

    solve

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -2,18 +2,26 @@ using System;
 
 static class LogLine
 {
-    public static string Message(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
-    }
+  public static string Message(string logLine)
+  {
+    //[level]: MESSAGE
+    //split on ':' and trim whitespace
+    string[] splitLine = logLine.Split(':', 2, StringSplitOptions.TrimEntries);
+    return splitLine[1];
+  }
 
-    public static string LogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
-    }
+  public static string LogLevel(string logLine)
+  {
+    string[] splitLine = logLine.Split(':', 2, StringSplitOptions.TrimEntries);
+    //cut first and last chars then to lowercase
+    return splitLine[0].Trim(['[', ']']).ToLower();
+  }
 
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
+  public static string Reformat(string logLine)
+  {
+    string message = Message(logLine);
+    string level = LogLevel(logLine);
+
+    return $"{message} ({level})";
+  }
 }
