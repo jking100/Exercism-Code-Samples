4d4a60626182a4166613d23745633271a337a2a0
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-19 18:29:07 -0600

    solve

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -2,18 +2,40 @@ using System;
 
 static class AssemblyLine
 {
-    public static double SuccessRate(int speed)
+  public static double SuccessRate(int speed)
+  {
+    /*
+     - `0`: 0% success rate.
+- `1` to `4`: 100% success rate.
+- `5` to `8`: 90% success rate.
+- `9`: 80% success rate.
+- `10`: 77% success rate.
+     */
+    if (speed == 0)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+      return 0;
     }
-    
-    public static double ProductionRatePerHour(int speed)
+    else if (speed >= 1 && speed <= 4)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+      return 1.0;
     }
+    else if (speed >= 5 && speed <= 8)
+    {
+      return .90;
+    }
+    else if (speed == 9)
+    {
+      return .80;
+    }
+    else
+    {
+      return .77;
+    }
+  }
 
-    public static int WorkingItemsPerMinute(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
-    }
+  public static double ProductionRatePerHour(int speed) =>
+    (221 * speed * SuccessRate(speed));
+
+  public static int WorkingItemsPerMinute(int speed) =>
+    (int)(ProductionRatePerHour(speed) / 60);
 }
