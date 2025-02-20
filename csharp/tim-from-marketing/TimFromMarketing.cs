e3d44aa3981ff8add54653fa66c973f428d8e638
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-19 20:00:31 -0600

    solve test task 1

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -1,9 +1,14 @@
 using System;
 
+/*
+  Badge.Print(734, "Ernest Johnny Payne", "Strategic Communication");
+  // => "[734] - Ernest Johnny Payne - STRATEGIC COMMUNICATION"
+*/
 static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
-    }
+  public static string Print(int? id, string name, string? department)
+  {
+    return $"[{id}] - {name} - {department?.ToUpper()}";
+    //throw new NotImplementedException("Please implement the (static) Badge.Print() method");
+  }
 }
