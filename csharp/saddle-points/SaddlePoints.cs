2c3c12c94d701b990f19b7b469317aeb1b5ccb2f
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-20 09:03:18 -0600

    optomize solution

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -50,17 +50,43 @@ public static class SaddlePoints
     {
         int rowLen = matrix.GetLength(0);
         int colLen = matrix.GetLength(1);
-        /*
+
         int[] rowMax = new int[rowLen];
-        int[] colMax = new int[colLen];
-        */
+        int[] colMin = new int[colLen];
+
+        //precompute max values per row
+        for (int i = 0; i < rowLen; i++)
+        {
+            rowMax[i] = matrix[i, 0];
+            for (int j = 0; j < colLen; j++)
+            {
+                if (matrix[i, j] > rowMax[i]) rowMax[i] = matrix[i, j];
+            }
+        }
+        //precompute min values per col
+        for (int i = 0; i < colLen; i++)
+        {
+            colMin[i] = matrix[0, i];
+            for (int j = 0; j < rowLen; j++)
+            {
+                if (matrix[j, i] < colMin[i]) colMin[i] = matrix[j, i];
+            }
+        }
+
+        //iterate thru whole array
+        //check if each member is equal to the max and min of its row
+        //if so yield its coords
         for (int i = 0; i < rowLen; i++)
         {
             for (int j = 0; j < colLen; j++)
             {
-                if (valid(matrix, i, j)) yield return (i + 1, j + 1);
+                if (matrix[i, j] == rowMax[i] && matrix[i, j] == colMin[j])
+                {
+                    yield return (i + 1, j + 1);
+                }
             }
         }
+        Console.WriteLine("");
         //throw new NotImplementedException("You need to implement this method.");
     }
 }
