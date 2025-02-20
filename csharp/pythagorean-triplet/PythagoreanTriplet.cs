f72143f40bd405dba121306f79d7cdac11464c21
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-19 22:41:18 -0600

    solve test task 1

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,6 +5,33 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        List<(int a, int b, int c)> triplets = [];
+        //given
+        //a2+b2=c2
+        //a<b<c
+        //sum = a+b+c
+
+        //deduce
+        //all are >= 1
+        //a+b > c
+
+        //ideas:
+        //generate all combos of a and b and c that add to sum
+
+        int c;
+
+        for (int a = 1; 2 * a <= (sum - 2 * a); a++)
+        {
+            for (int b = a; b < (sum - (a + b)); b++)
+            {
+                c = (int)Math.Pow((sum - (a + b)), 2);
+                if (a * a + b * b == c)
+                {
+                    triplets.Add((a, b, (sum - (a + b))));
+                }
+            }
+        }
+
+        return triplets.ToArray();
     }
 }
