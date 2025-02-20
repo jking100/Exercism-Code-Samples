2121ccf397aa3c6981799d2ae7274dee24df3b22
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-20 08:40:10 -0600

    initial solve

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -1,11 +1,66 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 public static class SaddlePoints
 {
+    public static bool IsTallestInRow(int[,] matrix, int x, int y)
+    {
+        int rowLen = matrix.GetLength(0);
+        int colLen = matrix.GetLength(1);
+
+        for (int i = 0; i < colLen; i++)
+        {
+            if (i == y) continue;
+            if (matrix[x, i] > matrix[x, y])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static bool IsShortestInCol(int[,] matrix, int x, int y)
+    {
+        int rowLen = matrix.GetLength(0);
+        int colLen = matrix.GetLength(1);
+
+        for (int i = 0; i < rowLen; i++)
+        {
+            if (i == x) continue;
+            if (matrix[i, y] < matrix[x, y])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool valid(int[,] matrix, int x, int y)
+    {
+        if (IsShortestInCol(matrix, x, y) && IsTallestInRow(matrix, x, y))
+        {
+            return true;
+        }
+        return false;
+    }
+
     public static IEnumerable<(int, int)> Calculate(int[,] matrix)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        int rowLen = matrix.GetLength(0);
+        int colLen = matrix.GetLength(1);
+        /*
+        int[] rowMax = new int[rowLen];
+        int[] colMax = new int[colLen];
+        */
+        for (int i = 0; i < rowLen; i++)
+        {
+            for (int j = 0; j < colLen; j++)
+            {
+                if (valid(matrix, i, j)) yield return (i + 1, j + 1);
+            }
+        }
+        //throw new NotImplementedException("You need to implement this method.");
     }
 }
--- csharp/saddle-points/SaddlePointsTests.cs
+++ csharp/saddle-points/SaddlePointsTests.cs
@@ -4,113 +4,143 @@ using Xunit;
 
 public class SaddlePointsTests
 {
-    [Fact]
-    public void Can_identify_single_saddle_point()
+  [Fact]
+  public void Test_IsTallestInRow()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
+            { 9, 8, 7, 5 },
+            { 5, 3, 2, 4 },
+            { 6, 6, 7, 4 }
+        };
+    Assert.True(SaddlePoints.IsTallestInRow(matrix, 1, 0));
+    Assert.False(SaddlePoints.IsTallestInRow(matrix, 1, 1));
+    Assert.True(SaddlePoints.IsTallestInRow(matrix, 2, 2));
+  }
+
+  [Fact]
+  public void Test_IsShortestInCol()
+  {
+    int[,] matrix =
+    {
+            { 9, 8, 7, 5 },
+            { 5, 3, 2, 4 },
+            { 6, 6, 7, 4 }
+        };
+    Assert.True(SaddlePoints.IsShortestInCol(matrix, 1, 0));
+    Assert.True(SaddlePoints.IsShortestInCol(matrix, 1, 1));
+    Assert.False(SaddlePoints.IsShortestInCol(matrix, 2, 2));
+    Assert.True(SaddlePoints.IsShortestInCol(matrix, 1, 2));
+    Assert.False(SaddlePoints.IsShortestInCol(matrix, 0, 0));
+  }
+
+  [Fact]
+  public void Can_identify_single_saddle_point()
+  {
+    int[,] matrix =
+    {
             { 9, 8, 7 },
             { 5, 3, 2 },
             { 6, 6, 7 }
         };
-        HashSet<(int, int)> expected = [(2, 1)];
-        Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
-    }
+    HashSet<(int, int)> expected = [(2, 1)];
+    Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_that_empty_matrix_has_no_saddle_points()
+  [Fact]
+  public void Can_identify_that_empty_matrix_has_no_saddle_points()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
         };
-        Assert.Empty(SaddlePoints.Calculate(matrix));
-    }
+    Assert.Empty(SaddlePoints.Calculate(matrix));
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_lack_of_saddle_points_when_there_are_none()
+  [Fact]
+  public void Can_identify_lack_of_saddle_points_when_there_are_none()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
             { 1, 2, 3 },
             { 3, 1, 2 },
             { 2, 3, 1 }
         };
-        Assert.Empty(SaddlePoints.Calculate(matrix));
-    }
+    Assert.Empty(SaddlePoints.Calculate(matrix));
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_multiple_saddle_points_in_a_column()
+  [Fact]
+  public void Can_identify_multiple_saddle_points_in_a_column()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
             { 4, 5, 4 },
             { 3, 5, 5 },
             { 1, 5, 4 }
         };
-        HashSet<(int, int)> expected = [(1, 2), (2, 2), (3, 2)];
-        Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
-    }
+    HashSet<(int, int)> expected = [(1, 2), (2, 2), (3, 2)];
+    Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_multiple_saddle_points_in_a_row()
+  [Fact]
+  public void Can_identify_multiple_saddle_points_in_a_row()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
             { 6, 7, 8 },
             { 5, 5, 5 },
             { 7, 5, 6 }
         };
-        HashSet<(int, int)> expected = [(2, 1), (2, 2), (2, 3)];
-        Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
-    }
+    HashSet<(int, int)> expected = [(2, 1), (2, 2), (2, 3)];
+    Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_saddle_point_in_bottom_right_corner()
+  [Fact]
+  public void Can_identify_saddle_point_in_bottom_right_corner()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
             { 8, 7, 9 },
             { 6, 7, 6 },
             { 3, 2, 5 }
         };
-        HashSet<(int, int)> expected = [(3, 3)];
-        Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
-    }
+    HashSet<(int, int)> expected = [(3, 3)];
+    Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_saddle_points_in_a_non_square_matrix()
+  [Fact]
+  public void Can_identify_saddle_points_in_a_non_square_matrix()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
             { 3, 1, 3 },
             { 3, 2, 4 }
         };
-        HashSet<(int, int)> expected = [(1, 3), (1, 1)];
-        Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
-    }
+    HashSet<(int, int)> expected = [(1, 3), (1, 1)];
+    Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_that_saddle_points_in_a_single_column_matrix_are_those_with_the_minimum_value()
+  [Fact]
+  public void Can_identify_that_saddle_points_in_a_single_column_matrix_are_those_with_the_minimum_value()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
             { 2 },
             { 1 },
             { 4 },
             { 1 }
         };
-        HashSet<(int, int)> expected = [(2, 1), (4, 1)];
-        Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
-    }
+    HashSet<(int, int)> expected = [(2, 1), (4, 1)];
+    Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
+  }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_identify_that_saddle_points_in_a_single_row_matrix_are_those_with_the_maximum_value()
+  [Fact]
+  public void Can_identify_that_saddle_points_in_a_single_row_matrix_are_those_with_the_maximum_value()
+  {
+    int[,] matrix =
     {
-        int[,] matrix =
-        {
             { 2, 5, 3, 5 }
         };
-        HashSet<(int, int)> expected = [(1, 2), (1, 4)];
-        Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
-    }
+    HashSet<(int, int)> expected = [(1, 2), (1, 4)];
+    Assert.Equal(expected, SaddlePoints.Calculate(matrix).ToHashSet());
+  }
 }
