d000d2f0db8a98a67157167e0891ed1751a8d0de
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-19 19:13:56 -0600

    solve test task 1

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,12 +1,16 @@
 using System;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+  // TODO: define the 'SubstringAfter()' extension method on the `string` type
+  public static string SubstringAfter(this string str, string delim)
+  {
+    return str.Split(delim)[1];
+  }
 
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
-    // TODO: define the 'Message()' extension method on the `string` type
+  // TODO: define the 'SubstringBetween()' extension method on the `string` type
 
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+  // TODO: define the 'Message()' extension method on the `string` type
+
+  // TODO: define the 'LogLevel()' extension method on the `string` type
 }
