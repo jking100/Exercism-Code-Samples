238047d61c59f2875db19193d653275063e6e387
Joseph King <173524547+jking100@users.noreply.github.com>
2025-02-19 18:49:07 -0600

    solve

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,13 +2,19 @@ using System;
 
 public static class PhoneNumber
 {
-    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
-    }
+  /*
+   * 1. An indication of whether the number has a New York dialing code ie. 212 as the first 3 digits
+2. An indication of whether the number is fake having 555 as a prefix code in positions 5 to 7 (numbering from 1)
+3. The last 4 digits of the number.
+  */
+  public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+  {
+    string[] phoneParts = phoneNumber.Split('-');
+    return ((phoneParts[0] == "212"), (phoneParts[1] == "555"), (phoneParts[2]));
+  }
 
-    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
-    }
+  public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+  {
+    return phoneNumberInfo.IsFake;
+  }
 }
