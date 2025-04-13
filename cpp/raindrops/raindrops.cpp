296d829eb5ee9b070bf9ca86a467e4430fe66ab0
Joseph King <173524547+jking100@users.noreply.github.com>
2025-04-13 10:45:42 -0500

    init local

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -2,19 +2,19 @@
 #include <string>
 
 namespace raindrops {
-    std::string convert (int num) {
-        std::string sound{};
+	std::string convert(int num) {
+		std::string sound{};
 
-        if (num % 3 == 0) {
-            sound.append("Pling");
-        }
-        if (num % 5 == 0) {
-            sound.append("Plang");
-        }
-        if (num % 7 == 0) {
-            sound.append("Plong");
-        }
+		if (num % 3 == 0) {
+			sound.append("Pling");
+		}
+		if (num % 5 == 0) {
+			sound.append("Plang");
+		}
+		if (num % 7 == 0) {
+			sound.append("Plong");
+		}
 
-        return sound.empty() ? std::to_string(num) : sound;
-    }
+		return sound.empty() ? std::to_string(num) : sound;
+	}
 }  // namespace raindrops
